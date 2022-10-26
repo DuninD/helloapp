4a264473d62dc99b804a2f907d9bc8220eaf949c
white <white@example.com>
2022-10-26 23:27:29 +0500

    Greet white

--- Git-actually/ConsoleApp1/Program.cs
+++ Git-actually/ConsoleApp1/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("hello");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("hello");
+    }
+    public static void greetWhite()
+    {
+        Console.WriteLine("hellowhite");
+    }
+}
