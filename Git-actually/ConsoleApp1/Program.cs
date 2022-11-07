cc5c7a4d7482542543cb25933cd85018c54a7fb3
black <black@example.com>
2022-11-07 13:39:50 +0500

    hello everyone

--- Git-actually/ConsoleApp1/Program.cs
+++ Git-actually/ConsoleApp1/Program.cs
@@ -1,12 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
+=======
+
+>>>>>>> 5484c30 (hello everyone)
 class Program
 {
     static void Main()
     {
+<<<<<<< HEAD
         Console.WriteLine("hello");
     }
     public static void greetWhite()
     {
         Console.WriteLine("hellowhite");
+=======
+        Console.WriteLine("hello everyone");
+>>>>>>> 5484c30 (hello everyone)
     }
 }
